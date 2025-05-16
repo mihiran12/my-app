811e16c835da283a388df7272bcbea105bfb7104
mihiran12 <mihiran935@gmail.com>
2025-05-16 10:46:46 +0530

    this is second commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <h1>Hello, React!</h1>
-      <p>This is a basic React app.</p>
+      <p>This is a basic React app with CI through github actions</p>
     </div>
   );
 }
